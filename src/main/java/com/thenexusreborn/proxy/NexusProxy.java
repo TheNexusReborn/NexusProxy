ed898c9fc6301ddbc856cc54268e0749ea7e904d
Firestar311 <slaterandrew0398@gmail.com>
2022-04-29 04:04:52 -0500

    Made it so that ranks sync properly

--- src/main/java/com/thenexusreborn/proxy/NexusProxy.java
+++ src/main/java/com/thenexusreborn/proxy/NexusProxy.java
@@ -1,6 +1,7 @@
 package com.thenexusreborn.proxy;
 
 import com.thenexusreborn.api.NexusAPI;
+import com.thenexusreborn.api.player.*;
 import com.thenexusreborn.proxy.api.ProxyPlayerManager;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.config.*;
@@ -8,6 +9,8 @@ import net.md_5.bungee.config.*;
 import java.io.*;
 import java.nio.file.Files;
 import java.sql.*;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class NexusProxy extends Plugin {
     private Configuration config;
@@ -34,6 +37,22 @@ public class NexusProxy extends Plugin {
         getProxy().getPluginManager().registerListener(this, (ProxyPlayerManager) NexusAPI.getApi().getPlayerManager());
         
         getProxy().registerChannel("nexus");
+        
+        getProxy().getScheduler().schedule(this, () -> {
+            PlayerManager playerManager = NexusAPI.getApi().getPlayerManager();
+            for (NexusPlayer player : playerManager.getPlayers().values()) {
+                try (Connection connection = getConnection(); Statement statement = connection.createStatement()) {
+                    ResultSet resultSet = statement.executeQuery("select ranks from players where uuid='" + player.getUniqueId() + "';");
+                    if (resultSet.next()) {
+                        String rawRanks = resultSet.getString("ranks");
+                        Map<Rank, Long> ranks = NexusAPI.getApi().getDataManager().parseRanks(rawRanks);
+                        player.setRanks(ranks);
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, 1L, 1L, TimeUnit.SECONDS);
     }
     
     public void saveDefaultConfig() {
