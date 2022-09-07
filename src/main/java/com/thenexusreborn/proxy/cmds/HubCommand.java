d0c8645f93c7da7f513869a31714a439bedbba49
Firestar311 <slaterandrew0398@gmail.com>
2022-09-07 14:40:12 -0500

    Improved the hub command

--- src/main/java/com/thenexusreborn/proxy/cmds/HubCommand.java
+++ src/main/java/com/thenexusreborn/proxy/cmds/HubCommand.java
@@ -1,10 +1,13 @@
 package com.thenexusreborn.proxy.cmds;
 
+import com.thenexusreborn.api.NexusAPI;
+import com.thenexusreborn.api.server.ServerInfo;
 import net.md_5.bungee.api.*;
 import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
+
+import java.util.*;
 
 public class HubCommand extends Command {
     public HubCommand() {
@@ -19,11 +22,27 @@ public class HubCommand extends Command {
         }
         
         ProxiedPlayer player = (ProxiedPlayer) sender;
-        for (ServerInfo server : ProxyServer.getInstance().getServers().values()) {
-            if (server.getName().equalsIgnoreCase("h1")) {
-                player.connect(server);
-                player.sendMessage(new ComponentBuilder("Sent you to the hub").color(ChatColor.YELLOW).create());
-                break;
+        List<com.thenexusreborn.api.server.ServerInfo> hubServers = new ArrayList<>();
+        for (com.thenexusreborn.api.server.ServerInfo server : NexusAPI.getApi().getServerManager().getServers()) {
+            if (server.getType().equalsIgnoreCase("hub")) {
+                hubServers.add(server);
+            }
+        }
+        
+        findHub(player, hubServers);
+    }
+    
+    private void findHub(ProxiedPlayer player, List<ServerInfo> hubServers) {
+        for (ServerInfo hubServer : hubServers) {
+            if (hubServer.getState().equalsIgnoreCase("online")) {
+                net.md_5.bungee.api.config.ServerInfo serverInfo = ProxyServer.getInstance().getServerInfo(hubServer.getName());
+                player.connect(serverInfo, (status, throwable) -> {
+                    if (!status) {
+                        findHub(player, hubServers);
+                    } else {
+                        player.sendMessage(new ComponentBuilder("Sent you to the hub").color(ChatColor.YELLOW).create());
+                    }
+                });
             }
         }
     }
